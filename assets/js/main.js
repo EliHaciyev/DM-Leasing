8831dcd5bb0507b8fc318b352ec8547ee996d09b
EliHaciyev <ali.haciyev457@gmail.com>
2024-08-02 16:39:54 +0400

    The page is fully customized only adaptability remains

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,34 @@ const swiper = new Swiper(".swiper", {
     disableOnInteraction: false,
   },
 });
+
+const tabItems = document.querySelectorAll('.tabs__btn-item');
+const tabContents = document.querySelectorAll('.tabs__content-item');
+
+tabItems.forEach(function(element) {
+  element.addEventListener('click', open);
+});
+
+function open(event) {
+  const tabTarget = event.currentTarget;
+  const button = tabTarget.dataset.button;
+
+  // Убираем активный класс у всех табов
+  tabItems.forEach(function(item) {
+    item.classList.remove('tabs__btn-item--active');
+  });
+
+  // Добавляем активный класс текущему табу
+  tabTarget.classList.add('tabs__btn-item--active');
+
+  // Убираем активный класс у всех контентных элементов
+  tabContents.forEach(function(item) {
+    item.classList.remove('tabs__content-item--active');
+  });
+
+  // Добавляем активный класс контенту, соответствующему текущему табу
+  const targetContent = document.querySelector(`#${button}`);
+  if (targetContent) {
+    targetContent.classList.add('tabs__content-item--active');
+  }
+}
